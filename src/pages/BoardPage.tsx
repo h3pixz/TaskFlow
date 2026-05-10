5ad785866cb8be6823ed5978a7ad8d0cdb506daf
Vlad Rymarchik <vladrymarchik@gmail.com>
2026-05-10 17:28:42 +0300

    add: header in board page without board name

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,3 +1,51 @@
+import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 export function BoardPage() {
-  return <></>;
+  const navigate = useNavigate();
+  const [userEmail] = useState(() => localStorage.getItem("userEmail") || "");
+  const [boardTitle] = useState(() => localStorage.getItem("boardTitle") || "");
+  const [isEditingTitle, setIsEditingTitle] = useState(false);
+
+  useEffect(() => {
+    const email = localStorage.getItem("userEmail");
+    if (!email) {
+      navigate("/");
+      return;
+    }
+  });
+
+  return (
+    <div className="min-h-screen" style={{ backgroundColor: "#121212" }}>
+      <div
+        className="px-8 py-4 border-b flex items-center justify-between"
+        style={{ borderColor: "#2a2a2a" }}
+      >
+        <div className="flex items-center gap-4">
+          <button
+            className="cursor-pointer transition-colors"
+            style={{ color: "#9CA3AF" }}
+            onMouseEnter={(e) => {
+              e.currentTarget.style.color = "#FFFFFF";
+            }}
+            onMouseLeave={(e) => {
+              e.currentTarget.style.color = "#9CA3AF";
+            }}
+            onClick={() => navigate("/dashboard")}
+          >
+            <ArrowLeft size={24} />
+          </button>
+
+          {isEditingTitle ? (
+            <h1>lol</h1>
+          ) : (
+            <h1>{boardTitle}</h1>
+          )}
+
+        </div>
+        <p className="text-sm" style={{ color: "#a0a0a0" }}>{userEmail}</p>
+      </div>
+    </div>
+  );
 }
